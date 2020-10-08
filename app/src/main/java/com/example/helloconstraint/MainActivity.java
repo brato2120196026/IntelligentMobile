0cf4c7e2e01eebe88032a3e82d8daa3f2a2d80cc
brato2120196026 <71314594+brato2120196026@users.noreply.github.com>
2020-10-08 20:00:42 +0800

    Adding Homework for Android fundamentals 01.2 Part B

--- app/src/main/java/com/example/helloconstraint/MainActivity.java
+++ app/src/main/java/com/example/helloconstraint/MainActivity.java
@@ -1,21 +1,32 @@
-package com.example.hellotoast;
+package com.example.helloconstraint;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.hellotoast.R;
+
 public class MainActivity extends AppCompatActivity {
-    private int mCount = 0;
+    private int mCount = 0, resetZero = 0;
     private TextView mShowCount;
+    private Button button_zero, button_count, button_toast;
+    private int pink;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mShowCount = (TextView) findViewById(R.id.show_count);
+        button_count= (Button) findViewById(R.id.button_count);
+        button_toast = (Button)findViewById(R.id.button_toast);
+        button_zero = (Button)findViewById(R.id.button_zero);
+
     }
 
 //Toast method
@@ -29,5 +40,12 @@ public class MainActivity extends AppCompatActivity {
         ++mCount;
         if (mShowCount != null)
             mShowCount.setText(Integer.toString(mCount));
+            button_zero.setBackgroundColor(getResources().getColor(R.color.pink));;
+    }
+
+    public void setZero(View view) {
+        mShowCount.setText(Integer.toString(mCount=0));
+        button_zero.setBackgroundColor(Color.GRAY);
+
     }
 }
