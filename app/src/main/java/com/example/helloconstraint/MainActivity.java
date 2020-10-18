da59130a1e644b78a45f983c47c86f5cf0b7ec04
brato2120196026 <71314594+brato2120196026@users.noreply.github.com>
2020-10-19 04:14:51 +0800

    Adding HelloConstraint Homework

--- app/src/main/java/com/example/helloconstraint/MainActivity.java
+++ app/src/main/java/com/example/helloconstraint/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.helloconstraint;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
@@ -14,7 +15,7 @@ import com.example.hellotoast.R;
 public class MainActivity extends AppCompatActivity {
     private int mCount = 0, resetZero = 0;
     private TextView mShowCount;
-    private Button button_zero, button_count, button_toast;
+    private Button button_zero, button_count;
     private int pink;
 
 
@@ -24,25 +25,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mShowCount = (TextView) findViewById(R.id.show_count);
         button_count= (Button) findViewById(R.id.button_count);
-        button_toast = (Button)findViewById(R.id.button_toast);
         button_zero = (Button)findViewById(R.id.button_zero);
 
     }
 
 //Toast method
     public void showToast(View view) {
-        Toast toast = Toast.makeText(this, R.string.toast_message, Toast.LENGTH_SHORT);
-        toast.show();
+         Toast.makeText(this, R.string.toast_message, Toast.LENGTH_SHORT).show();
     }
 
 //Count method
+    @SuppressLint("SetTextI18n")
     public void countUp(View view) {
         ++mCount;
         if (mShowCount != null)
             mShowCount.setText(Integer.toString(mCount));
-            button_zero.setBackgroundColor(getResources().getColor(R.color.pink));;
+            button_zero.setBackgroundColor(getResources().getColor(R.color.pink));
     }
 
+    @SuppressLint("SetTextI18n")
     public void setZero(View view) {
         mShowCount.setText(Integer.toString(mCount=0));
         button_zero.setBackgroundColor(Color.GRAY);
