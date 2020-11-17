ef345031e949f8ed9e03823585498b45def5c74a
brato2120196026 <71314594+brato2120196026@users.noreply.github.com>
2020-11-18 04:28:38 +0800

    Adding a Checkboxes Homework App

---
+++ app/README.md
@@ -0,0 +1,26 @@
+# Checkboxes Homework - Solution Code
+
+
+## Pre-requisites
+To run this app you should be familiar with creating and running apps in Android Studio.
+
+## Getting Started
+
+1. Download the code.
+2. Open the code in Android Studio.
+3. Run the app.
+
+## The Homework app should contain the following features:
+
+
+## Result
+
+#### Checkboxes Homework Screenshots
+
+![](CheckboxesHomework1.png)
+
+
+![](CheckboxesHomework2.png)
+
+
+![](CheckboxesHomework3.png)
---
+++ app/src/main/java/com/example/checkboxes/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.checkboxes;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    CheckBox CheckBox_1, CheckBox_2,
+            CheckBox_3, CheckBox_4,
+            CheckBox_5;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        CheckBox_1 = findViewById(R.id.checkBox_1);
+        CheckBox_2 = findViewById(R.id.checkBox_2);
+        CheckBox_3 = findViewById(R.id.checkBox_3);
+        CheckBox_4 = findViewById(R.id.checkBox_4);
+        CheckBox_5 = findViewById(R.id.checkBox_5);
+    }
+
+    public void onCheckBoxClicked(View v) {
+        String msg = "";
+
+        if (CheckBox_1.isChecked())
+            msg = msg + " Chocolate syrup ";
+        if (CheckBox_2.isChecked())
+            msg = msg + " Sprinkles";
+        if (CheckBox_3.isChecked())
+            msg = msg + " Crushed nuts";
+        if (CheckBox_4.isChecked())
+            msg = msg + " Cherries";
+        if (CheckBox_5.isChecked())
+            msg = msg + " Orio cookies crumbles";
+
+        Toast.makeText(this, msg + "are selected",
+                Toast.LENGTH_LONG).show();
+    }
+}
+
